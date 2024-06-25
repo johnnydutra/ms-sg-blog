9a5a583081b628ef64aa84ddbb644e346ccf2f9d
Johnny Dutra <johnny.dutra@gmail.com>
2024-06-25 15:11:22 -0300

    implements Moderation service

--- comments/index.js
+++ comments/index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -29,9 +29,10 @@ app.post('/posts/:id/comments', async (req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
-  })
+  });
 
   res.status(201).send(comments);
 });
